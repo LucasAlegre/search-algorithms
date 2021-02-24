7ba65c9498c5b34020c72e0f6ad7b23620883910
kovaleski100 <mfkovaleski@inf.ufrgs.br>
2021-02-24 20:41:56 -0300

    simple instance

--- Main.cpp
+++ Main.cpp
@@ -1,17 +1,76 @@
 #include <iostream>
+#include <string.h>
 #include "include/Algorithms/BreadthFirstSearch.h"
 
 using namespace std;
 
 int main(int argc, char** argv) {
-    State state(vector<int>({5,0,2,6,4,8,1,7,3}));
-    //State state(vector<int>({0,1,2,3,4,5,6,7,8}));
-    //cout << state.value << endl;
 
-    BreadthFirstSearch bfs = BreadthFirstSearch();
+    vector<int> game;
+    vector<State> games;
+    //State state(vector<int>({0,1,2,3,4,5,6,7,8}));///S*
+    ///State state(game);////S0
 
-    bfs.run(state);
-    bfs.print();
+    int i = 2;
 
+    while(i<argc)
+    {
+        game.push_back(stoi(argv[i]));
+        i++;
+    }
+    
+    games.push_back(game);
+
+    cout << games[0].value << endl;
+
+
+    /*
+    vector<string> input;
+    while(i<argc)
+    {
+        input.push_back(argv[i]);
+        i++;
+    }
+
+    i =0;
+    while(i<argc)
+    {
+        if(input[i].find(',')!=string::npos)
+        {
+            input[i].
+            game.push_back(stoi(argv[i]));
+            games.push_back(game);
+            vector<int> game;
+        }
+        else{
+            game.push_back(stoi(argv[i]));
+        }
+        i++;
+    }
+    */
+    
+
+    if(strcmp(argv[1],"-bfs")==0)
+    {
+        BreadthFirstSearch bfs = BreadthFirstSearch();
+        bfs.run(games[0]);
+        bfs.print();
+    }
+    else if(strcmp(argv[1],"-idfs")==0)
+    {
+        ///function
+    }
+    else if(strcmp(argv[1],"-astar")==0)
+    {
+        ///function
+    }
+    else if(strcmp(argv[1],"-idastar")==0)
+    {
+        ///function
+    }
+    else if(strcmp(argv[1],"-gbfs")==0)
+    {
+        ///function
+    }
     return 0;
 }
