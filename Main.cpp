15f66d863b24a2d992d91442a6a325c6433b5c0f
kovaleski100 <mfkovaleski@inf.ufrgs.br>
2021-02-27 15:27:50 -0300

    command-line argument ok

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 #include <string.h>
+#include <sstream>
 #include "include/Algorithms/BreadthFirstSearch.h"
 
 using namespace std;
+
+vector<string> split(string input, string delimiter)
+{
+    vector<string> jogos;
+
+    size_t pos = 0;
+    std::string token;
+    while ((pos = input.find(delimiter)) != std::string::npos) {
+        token = input.substr(0, pos);
+        input.erase(0, pos + delimiter.length());
+        jogos.push_back(token);
+    }
+    jogos.push_back(input);
+    return jogos;
+}
+
 
 int main(int argc, char** argv) {
 
@@ -13,48 +30,35 @@ int main(int argc, char** argv) {
 
     int i = 2;
 
-    while(i<argc)
+    string input ="";
+
+
+    for(int i=2; i<argc; i++)
     {
-        game.push_back(stoi(argv[i]));
-        i++;
-    }
-    
-    games.push_back(game);
-
-    cout << games[0].value << endl;
-
-
-    /*
-    vector<string> input;
-    while(i<argc)
-    {
-        input.push_back(argv[i]);
-        i++;
-    }
-
-    i =0;
-    while(i<argc)
-    {
-        if(input[i].find(',')!=string::npos)
+        input =argv[i];
+        if(input.find(",") == string::npos)
         {
-            input[i].
-            game.push_back(stoi(argv[i]));
+            game.push_back(stoi(input));
+        }
+        else
+        {
+            input = input.substr(0,input.size()-1);
+            game.push_back(stoi(input));
             games.push_back(game);
-            vector<int> game;
+            game.clear();
         }
-        else{
-            game.push_back(stoi(argv[i]));
-        }
-        i++;
+        
     }
-    */
-    
-
+    if(!game.empty())
+        games.push_back(game);
     if(strcmp(argv[1],"-bfs")==0)
     {
-        BreadthFirstSearch bfs = BreadthFirstSearch();
-        bfs.run(games[0]);
-        bfs.print();
+        for(int i = 0; i<games.size(); i++)
+        {
+            BreadthFirstSearch bfs = BreadthFirstSearch();
+            bfs.run(games[i]);
+            bfs.print();    
+        }
     }
     else if(strcmp(argv[1],"-idfs")==0)
     {
